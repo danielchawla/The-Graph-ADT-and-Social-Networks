3cb12325cf00098173d32017b6cd75d5584c4257
Annabelle Harvey <annabelle.ahrv@gmail.com>
2015-10-20 19:54:23 -0700

    intentional merge conflict

--- src/twitterAnalysis/TwitterAnalysis.java
+++ src/twitterAnalysis/TwitterAnalysis.java
@@ -6,10 +6,11 @@ public class TwitterAnalysis {
     public static void main (String inFile, String outFile){
         Scanner file = new Scanner(System.in);
         System.out.print("File name (blank for empty): ");
-        String url = file.nextLine().trim();
+        String purl = file.nextLine().trim();
 
         // TODO: Duplicate queries should be ignored.
         // TODO: All queries end with ?; lines in the query input file that do not end with a ? can also be ignored.
+        
         
     }
 }
