ad1a17df4a4ef43aa5a1400701dbc863d9cbc1d1
Annabelle Harvey <annabelle.ahrv@gmail.com>
2015-10-20 18:05:19 -0700

    added method stubs to ListGraph

--- src/ca/ubc/ece/cpen221/mp3/graph/AdjacencyListGraph.java
+++ src/ca/ubc/ece/cpen221/mp3/graph/AdjacencyListGraph.java
@@ -1,6 +1,38 @@
 package ca.ubc.ece.cpen221.mp3.graph;
+import java.util.LinkedList;
+import java.util.List;
+
 import ca.ubc.ece.cpen221.mp3.staff.Graph;
+import ca.ubc.ece.cpen221.mp3.staff.Vertex;
 
 public class AdjacencyListGraph implements Graph {
 // TODO: Implement this class
+    public void addVertex(Vertex v){
+        
+    }
+
+    public void addEdge(Vertex v1, Vertex v2){
+        
+    }
+
+    public boolean edgeExists(Vertex v1, Vertex v2){
+        //dummy return
+        return false;
+        
+    }
+
+    public List<Vertex> getDownstreamNeighbors(Vertex v){
+        List<Vertex> dummyreturn = new LinkedList<Vertex>();
+        return dummyreturn;
+    }
+
+    public List<Vertex> getUpstreamNeighbors(Vertex v){
+        List<Vertex> dummyreturn = new LinkedList<Vertex>();
+        return dummyreturn;
+    }
+
+    public List<Vertex> getVertices(){
+        List<Vertex> dummyreturn = new LinkedList<Vertex>();
+        return dummyreturn;
+    }
 }
