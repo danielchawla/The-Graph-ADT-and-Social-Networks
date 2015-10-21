5758409ff8fa0893b821a3517be9d71bd8cd9e13
Annabelle Harvey <annabelle.ahrv@gmail.com>
2015-10-20 19:52:32 -0700

    Implemented adjacencylist graph

--- src/ca/ubc/ece/cpen221/mp3/graph/AdjacencyListGraph.java
+++ src/ca/ubc/ece/cpen221/mp3/graph/AdjacencyListGraph.java
@@ -1,38 +1,101 @@
 package ca.ubc.ece.cpen221.mp3.graph;
+
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import ca.ubc.ece.cpen221.mp3.staff.Graph;
 import ca.ubc.ece.cpen221.mp3.staff.Vertex;
 
 public class AdjacencyListGraph implements Graph {
-// TODO: Implement this class
-    public void addVertex(Vertex v){
-        
+    // TODO: Implement this class
+
+    private Map<Vertex, List<Vertex>> listGraph = new HashMap<Vertex, List<Vertex>>();
+
+    /**
+     * Adds a vertex to the graph.
+     *
+     * Precondition: v is not already a vertex in the graph
+     */
+    public void addVertex(Vertex v) {
+        List<Vertex> following = new LinkedList<Vertex>();
+        listGraph.put(v, following);
     }
 
-    public void addEdge(Vertex v1, Vertex v2){
-        
+    /**
+     * Adds an edge from v1 to v2.
+     *
+     * Precondition: v1 and v2 are vertices in the graph
+     */
+    public void addEdge(Vertex v1, Vertex v2) {
+        // get list mapped to v1 and add v2
+        // v1 follows v2
+        listGraph.get(v1).add(v2);
     }
 
-    public boolean edgeExists(Vertex v1, Vertex v2){
-        //dummy return
-        return false;
-        
+    /**
+     * Check if there is an edge from v1 to v2.
+     *
+     * Precondition: v1 and v2 are vertices in the graph Postcondition: return
+     * true iff an edge from v1 connects to v2
+     */
+    public boolean edgeExists(Vertex v1, Vertex v2) {
+        // check list mapped to v1 for v2
+        // does v1 follow v2?
+        return listGraph.get(v1).contains(v2);
     }
 
-    public List<Vertex> getDownstreamNeighbors(Vertex v){
-        List<Vertex> dummyreturn = new LinkedList<Vertex>();
-        return dummyreturn;
+    /**
+     * Get an array containing all downstream vertices adjacent to v.
+     *
+     * Precondition: v is a vertex in the graph
+     * 
+     * Postcondition: returns a list containing each vertex w such that there is
+     * an edge from v to w. The size of the list must be as small as possible
+     * (No trailing null elements). This method should return a list of size 0
+     * iff v has no downstream neighbors.
+     */
+    public List<Vertex> getDownstreamNeighbors(Vertex v) {
+        // return list mapped to v
+        // who does v follow
+        return listGraph.get(v);
+
     }
 
-    public List<Vertex> getUpstreamNeighbors(Vertex v){
-        List<Vertex> dummyreturn = new LinkedList<Vertex>();
-        return dummyreturn;
+    /**
+     * Get an array containing all upstream vertices adjacent to v.
+     *
+     * Precondition: v is a vertex in the graph
+     * 
+     * Postcondition: returns a list containing each vertex u such that there is
+     * an edge from u to v. The size of the list must be as small as possible
+     * (No trailing null elements). This method should return a list of size 0
+     * iff v has no upstream neighbors.
+     */
+    public List<Vertex> getUpstreamNeighbors(Vertex v) {
+        // return a list of followers
+
+        List<Vertex> upstreamNeighbours = new LinkedList<Vertex>();
+        for (Vertex vertex : listGraph.keySet()) {
+            if (listGraph.get(vertex).contains(v))
+                upstreamNeighbours.add(vertex);
+        }
+
+        return upstreamNeighbours;
+
     }
 
-    public List<Vertex> getVertices(){
-        List<Vertex> dummyreturn = new LinkedList<Vertex>();
-        return dummyreturn;
+    /**
+     * Get all vertices in the graph.
+     *
+     * Postcondition: returns a list containing all vertices in the graph. This
+     * method should return a list of size 0 iff the graph has no vertices.
+     */
+    public List<Vertex> getVertices() {
+        List<Vertex> allVertices = new LinkedList<Vertex>();
+        allVertices.addAll(listGraph.keySet());
+        return allVertices;
+
     }
 }
--- src/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
+++ src/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
@@ -1,31 +1,63 @@
 package ca.ubc.ece.cpen221.mp3.graph;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import ca.ubc.ece.cpen221.mp3.staff.Graph;
 import ca.ubc.ece.cpen221.mp3.staff.Vertex;
 
 public class Algorithms {
 
-	/**
-	 * *********************** Algorithms ****************************
-	 * 
-	 * Please see the README for the machine problem for a more detailed
-	 * specification of the behavior of each method that one should implement.
-	 */
+    /**
+     * *********************** Algorithms ****************************
+     * 
+     * Please see the README for the machine problem for a more detailed
+     * specification of the behavior of each method that one should implement.
+     */
 
-	/**
-	 * This is provided as an example to indicate that this method and other
-	 * methods should be implemented here.
-	 * 
-	 * You should write the specs for this and all other methods.
-	 * 
-	 * @param graph
-	 * @param a
-	 * @param b
-	 * @return
-	 */
-	public static int shortestDistance(Graph graph, Vertex a, Vertex b) {
-		// TODO: Implement this method and others
-		return 0;
-	}
+    /**
+     * This is provided as an example to indicate that this method and other
+     * methods should be implemented here.
+     * 
+     * You should write the specs for this and all other methods.
+     * 
+     * @param graph
+     * @param a
+     * @param b
+     * @return
+     */
+    
+    public static int shortestDistance(Graph graph, Vertex a, Vertex b) {
+        // TODO: Implement this method and others
+        return 0;
+    }
+
+    public static List<Vertex> breadthFirstSearch(Graph graph) {
+        // TODO: Implement this method and others
+        List<Vertex> dummyReturn = new LinkedList<Vertex>();
+        return dummyReturn;
+
+    }
+
+    public static List<Vertex> depthFirstSearch(Graph graph) {
+        // TODO: Implement this method and others
+        List<Vertex> dummyReturn = new LinkedList<Vertex>();
+        return dummyReturn;
+
+    }
+
+    public static List<Vertex> commonUpstreamVertices(Graph graph, Vertex a, Vertex b) {
+        // TODO: Implement this method and others
+        List<Vertex> dummyReturn = new LinkedList<Vertex>();
+        return dummyReturn;
+
+    }
+
+    public static List<Vertex> commonDownstreamVertices(Graph graph, Vertex a, Vertex b) {
+        // TODO: Implement this method and others
+        List<Vertex> dummyReturn = new LinkedList<Vertex>();
+        return dummyReturn;
+
+    }
 
 }
