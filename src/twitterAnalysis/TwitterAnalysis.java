d1b70fef8a46e301952c22603c6c5a017dffef6e
Daniel Chwala <danielchawla93@gmail.com>
2015-10-20 19:10:51 -0700

    updated twitter analysis

--- src/twitterAnalysis/TwitterAnalysis.java
+++ src/twitterAnalysis/TwitterAnalysis.java
@@ -1,5 +1,15 @@
 package twitterAnalysis;
+import java.util.Scanner;
 
 public class TwitterAnalysis {
+    
+    public static void main (String inFile, String outFile){
+        Scanner file = new Scanner(System.in);
+        System.out.print("File name (blank for empty): ");
+        String url = file.nextLine().trim();
 
+        // TODO: Duplicate queries should be ignored.
+        // TODO: All queries end with ?; lines in the query input file that do not end with a ? can also be ignored.
+        
+    }
 }
