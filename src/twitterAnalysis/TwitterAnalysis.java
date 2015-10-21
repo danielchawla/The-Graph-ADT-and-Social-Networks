577e7ed141d2a90ffa6a7e38a62782f4fa130f6d
Daniel Chwala <danielchawla93@gmail.com>
2015-10-20 19:54:18 -0700

    test

--- src/twitterAnalysis/TwitterAnalysis.java
+++ src/twitterAnalysis/TwitterAnalysis.java
@@ -6,7 +6,8 @@ public class TwitterAnalysis {
     public static void main (String inFile, String outFile){
         Scanner file = new Scanner(System.in);
         System.out.print("File name (blank for empty): ");
-        String url = file.nextLine().trim();
+        
+        String random = file.nextLine().trim();
 
         // TODO: Duplicate queries should be ignored.
         // TODO: All queries end with ?; lines in the query input file that do not end with a ? can also be ignored.
